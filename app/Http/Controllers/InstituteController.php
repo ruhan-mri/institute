680dab61f26e83cda0719d9ab9c31fe3fa893301
Ruhan Islam <ruhan.9mri@gmail.com>
2024-09-14 11:03:30 +0600

    14 september work in morning

---
+++ app/Http/Controllers/InstituteController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class InstituteController extends Controller
+{
+    public function index(){}
+
+    public function create_std(){
+        return view('users.create');
+    }
+
+    public function store_std(){}
+
+    public function edit_std(){}
+
+    public function update_std(){}
+
+    public function delete_std(){}
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\InstituteController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/users/create',[InstituteController::class,'create_std'])->name('users.create');
